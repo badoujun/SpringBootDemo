805a5acb9b291430d8e996e317ae2aa7d07e5859
liqinhao <979561583@qq.com>
2019-03-14 23:12:06 +0800

    新增,redis

--- src/main/java/com/web/demo/controller/TradeController.java
+++ src/main/java/com/web/demo/controller/TradeController.java
@@ -5,12 +5,12 @@ import com.terran4j.commons.api2doc.annotations.ApiComment;
 import com.web.demo.pojo.Page;
 import com.web.demo.pojo.Trade;
 import com.web.demo.service.TradeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.util.List;
 
 @Api2Doc(id = "TradeController", name = "交易接口")
@@ -19,13 +19,25 @@ import java.util.List;
 @RequestMapping(value = "/trade")
 public class TradeController {
 
-    @Resource
+    @Autowired
     TradeService tradeService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @ApiComment(value = "查询所有交易记录")
     @RequestMapping(name = "交易记录", value = "/list")
     @ResponseBody
     public List<Trade> selectTradeAll(@ApiComment(value = "分页对象",seeClass = Page.class)Page page){
-        return tradeService.select(page);
+        String request_key = "tradeList_" + page.getPageNum() + "_" + page.getPageSize();
+        List<Trade> list = (List<Trade>)redisTemplate.opsForValue().get(request_key);
+        if(list == null || list.size() == 0)
+        {
+            list = tradeService.select(page);
+            redisTemplate.opsForValue().set(request_key,list);
+            System.out.println("数据库查询");
+        }else{
+            System.out.println("redis查询");
+        }
+        return list;
     }
 }
